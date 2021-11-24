fc9f7d5dfd84440c15a71fea6e4392d7665aea24
Bintaaaa <bijantium.yt.32@gmail.com>
2021-11-25 02:02:20 +0700

    Menambahkan Tema dan Test Tema
    1. Mendisable flutter_lints
    2. test tema pada main
    3. menambahkan warna
    4. menambahkan tema font
    5. menambahkan library googlefont

---
+++ lib/style/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color softBlueColor = Color(0xFF75CFE3);
+Color softGreenColor = Color(0xFF81C9C0);
+Color darkGreenColor = Color(0xFF75B6AE);
+Color softGreyColor = Color(0xFFAEAEAE);
+Color greyColor = Color(0xFF848484);
+Color darkGreyColor = Color(0xFF555555);
+Color whiteColor = Color(0xFFffffff);
+Color yellowColor = Color(0xFFE5B874);
+Color redColor = Color(0xFFC25353);
---
+++ lib/style/text_theme.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:gampah_app/style/color.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final TextTheme myTexTheme = TextTheme(
+  headline5: GoogleFonts.poppins(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: darkGreyColor,
+  ),
+  headline6: GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: darkGreyColor,
+  ),
+  subtitle1: GoogleFonts.poppins(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: softGreenColor,
+  ),
+  bodyText1: GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: softGreyColor,
+  ),
+  bodyText2: GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: softGreyColor,
+  ),
+  caption: GoogleFonts.poppins(
+    fontSize: 12,
+    fontWeight: FontWeight.w300,
+    color: darkGreyColor,
+  ),
+  overline: GoogleFonts.poppins(
+    fontSize: 10,
+    fontWeight: FontWeight.w300,
+    color: darkGreyColor,
+  ),
+  button: GoogleFonts.poppins(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: whiteColor,
+  ),
+);
