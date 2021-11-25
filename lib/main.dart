96677fb9461d00c1d8eb23b98ecf409a02b396a8
Seiko Santana <santanaaseiko@gmail.com>
2021-11-25 23:24:45 +0700

    Changed temporary default widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:gampah_app/style/text_theme.dart';
 import 'package:gampah_app/ui/pages/page_get_started.dart';
 import 'package:gampah_app/ui/pages/page_home.dart';
+import 'package:gampah_app/ui/pages/page_test_widget.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(TestPage()); //TODO: change to MyApp on production-ready
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
