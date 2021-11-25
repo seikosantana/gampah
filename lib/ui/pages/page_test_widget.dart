70e3401dc286004f6b7aa89f6f95b528d27b6c55
Seiko Santana <santanaaseiko@gmail.com>
2021-11-25 23:18:39 +0700

    Changed default widget shown

--- lib/ui/pages/page_test_widget.dart
+++ lib/ui/pages/page_test_widget.dart
@@ -1,6 +1,9 @@
 //A page to test whatever widget you want to see
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gampah_app/main.dart';
 import 'package:gampah_app/ui/pages/page_get_started.dart';
+import 'package:gampah_app/ui/widgets/form_controls/gampah_text_field.dart';
 
 class TestPage extends StatefulWidget {
   @override
@@ -13,7 +16,7 @@ class TestPageState extends State<TestPage> {
   @override
   Widget build(BuildContext context) {
     // change the build content to whatever
-    return GetStartedPage();
+    return MyApp();
   }
 }
 
