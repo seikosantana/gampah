67111731230b20013fc8b26582fac9788c583247
Bintaaaa <bijantium.yt.32@gmail.com>
2021-12-06 23:59:34 +0700

    create funtionality of transaction in this folder

--- lib/ui/pages/page_transaction_register.dart
+++ lib/ui/pages/page_transaction_register.dart
@@ -1,11 +1,18 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:gampah_app/helper_functions.dart/geolocation.dart';
+import 'package:gampah_app/provider/transactions_provider.dart';
 import 'package:gampah_app/style/color.dart';
 import 'package:gampah_app/style/text_theme.dart';
+import 'package:gampah_app/ui/error/error.dart';
+import 'package:gampah_app/ui/error/error_gps.dart';
+import 'package:gampah_app/ui/error/success_transaction.dart';
 import 'package:gampah_app/ui/widgets/form_controls/gampah_text_field.dart';
 import 'package:gampah_app/ui/widgets/widget_toolbar.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 class RegisterTransaction extends StatefulWidget {
   const RegisterTransaction({Key? key}) : super(key: key);
@@ -16,6 +23,8 @@ class RegisterTransaction extends StatefulWidget {
 }
 
 class _RegisterTransactionState extends State<RegisterTransaction> {
+  TextEditingController addressController = TextEditingController(text: '');
+
   File? image;
   Future pickImage() async {
     final image = await ImagePicker().pickImage(source: ImageSource.camera);
@@ -36,6 +45,7 @@ class _RegisterTransactionState extends State<RegisterTransaction> {
         children: [
           GampahTextField(
             labelText: "Detail Alamat (Opsional)",
+            controller: addressController,
           ),
           SizedBox(
             height: 16,
@@ -77,12 +87,12 @@ class _RegisterTransactionState extends State<RegisterTransaction> {
     );
   }
 
-  Widget _customButton() {
+  Widget _customButton(Function() handle) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 24),
       height: 50,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: handle,
         style: ButtonStyle(
           backgroundColor: MaterialStateProperty.all<Color>(darkGreenColor),
           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
@@ -103,13 +113,50 @@ class _RegisterTransactionState extends State<RegisterTransaction> {
 
   @override
   Widget build(BuildContext context) {
+    TransactionProvider transactionProvider =
+        Provider.of<TransactionProvider>(context);
+    handleTransaction() async {
+      Position? currentPosition =
+          await determinePosition(locationNotEnabledCallback: () {
+        Navigator.pushNamed(context, ErrorGpsPage.routeName);
+      }, permissionDeniedCallback: () {
+        Navigator.pushNamed(context, ErrorPage.routeName);
+      });
+      bool result = await transactionProvider.addTransaction(
+          1,
+          addressController.text,
+          "success",
+          currentPosition!.latitude,
+          currentPosition.longitude,
+          image!.path);
+      print(currentPosition);
+      if (result) {
+        return Navigator.pushReplacementNamed(
+            context, TransactionSuccess.routeName);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: redColor,
+            content: Text(
+              'Transaksi Gagal dibuat!',
+              textAlign: TextAlign.center,
+            ),
+          ),
+        );
+      }
+    }
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [_toolbar(), _textField(context), _customButton()],
+            children: [
+              _toolbar(),
+              _textField(context),
+              _customButton(handleTransaction),
+            ],
           ),
         ),
       ),
