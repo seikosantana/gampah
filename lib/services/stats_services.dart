e724f87d42aa003759ea77982feffa45443699ee
Seiko Santana <santanaaseiko@gmail.com>
2021-12-16 10:53:29 +0700

    Created stats services

---
+++ lib/services/stats_services.dart
@@ -0,0 +1,7 @@
+import 'package:http/http.dart' as http;
+
+class StatServices {
+  Future<int?> getDriversCount() async {}
+  Future<int?> getContributors() async {}
+  Future<int?> getFinishedTransactions() async {}
+}
