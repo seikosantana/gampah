6e4da1aa4072e46966a8199823e9cb31ed471cd0
Seiko Santana <santanaaseiko@gmail.com>
2021-12-16 13:31:56 +0700

    Added number postfix extension

--- lib/helper_functions.dart/extensions.dart
+++ lib/helper_functions.dart/extensions.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 extension StringCasingExtension on String {
   String toCapitalized() =>
       length > 0 ? '${this[0].toUpperCase()}${substring(1)}' : '';
@@ -5,4 +7,25 @@ extension StringCasingExtension on String {
       .split(" ")
       .map((str) => str.toCapitalized())
       .join(" ");
+}
+
+extension NumberSIPrefix on int {
+  static List<String> symbolsSI = [
+    "",
+    "k",
+    "M",
+    "G",
+    "T",
+    "P",
+    "E",
+  ];
+
+  double log10(num x) => log(x) / ln10;
+
+  String toSIPrefixed(int digits) {
+    int tier = (log10(this).toDouble() / 3).floor();
+    int scale = pow(10, tier * 3).toInt();
+    double scaled = this / scale;
+    return scaled.toStringAsFixed(digits) + symbolsSI[tier];
+  }
 }
