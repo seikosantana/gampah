872eef772f30195dcad0448b1160fb5ea2845db9
Seiko Santana <santanaaseiko@gmail.com>
2021-12-16 11:05:14 +0700

    implemented getDriversCount

--- lib/services/stats_services.dart
+++ lib/services/stats_services.dart
@@ -1,7 +1,22 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class StatServices {
-  Future<int?> getDriversCount() async {}
+  final String baseUrl = "https://shamo.tanpabatasgroup.com/api/stats";
+
+  Future<int?> getDriversCount() async {
+    int? count;
+    try {
+      var response = await http.get(Uri.parse("$baseUrl/drivers"));
+      Map<String, dynamic> body = jsonDecode(response.body);
+      count = body["data"]["count"];
+    } catch (err) {
+      throw Exception("Something went wrong in getting driver statistics");
+    }
+    return count;
+  }
+
   Future<int?> getContributors() async {}
   Future<int?> getFinishedTransactions() async {}
 }
